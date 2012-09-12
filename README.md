0e3fa6d01e70f1a5e2c10865725f0b6fdeefa251
Yoav Weiss <yoav@yoav.ws>
2012-09-12 23:58:06 +0200

    Better test page

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-css_js_parse_delay_test
+CSS JS parse delay test
 =======================
 
-This is a short Python HTTP server used to test if delaying CSS and JS objects is delaying their parsing &amp; execution
+This is a short Python HTTP server used to test if delaying CSS and JS objects is delaying their parsing &amp; execution.
+Basically, run the server and browse to its IP on port 8002 (so
+http://127.0.0.1:8002 if you're running it locally).
+
+The test will indicate if CSS and JS parsing was delayed
--- delay_test.py
+++ delay_test.py
@@ -11,25 +11,53 @@ html_content = """
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <script>
+        (function(){
+            var counter = 0;
+            var gotCSS = false;
+            var gotJS = false;
+            setInterval(function(){
+                counter++;
+                if(counter < 12){
+                    var result = document.getElementById("result");
+                    if(!gotCSS && getComputedStyle(result)["color"] != "rgb(0, 0, 0)"){
+                        console.log("Got CSS parsed at " + counter + " seconds");
+                        gotCSS = true;
+                        if(counter > 2){
+                            result.innerHTML += "CSS parsing was delayed";
+                        }
+                    }
+                    if(!gotJS && window.testing123){
+                        console.log("Got JS parsed at " + counter + " seconds");
+                        gotJS = true;
+                        if(counter > 6){
+                            result.innerHTML += "<br>JS parsing was delayed";
+                        }
+                    }
+                }
+            }, 1000);
+            })();
+        </script>
         <link href="test.css" rel="stylesheet">
     </head>
     <body>
-        This is a test page. Really!!!!!!! 
+        <p id="result"></p>
         <script src="test.js" type="text/javascript"></script>
     </body>
 </html>
 """
 
 css_content = """
-body{background-color: blue}
+#result{color: blue}
 /* This is just a content to blzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
  * zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
  * zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz */
 
-body{background-color: red}
 """ 
 
 js_content = """
+window.testing123 = true;
 console.log("This is 1st");
 console.timeStamp("bla");
 /* This comment is here to take some space. GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGg
