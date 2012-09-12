d9d8dcc6fee8f61f6587df6a379dc0ad981cac0f
Yoav Weiss <yoav@yoav.ws>
2012-09-12 21:49:19 +0200

    initial version of CSS/JS delay test

---
+++ delay_test.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+""" Test the effect of delay on CSS and JS """
+
+__version__ = "1.6"
+
+import BaseHTTPServer
+from SocketServer import ThreadingMixIn
+import time
+
+html_content = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="test.css" rel="stylesheet">
+    </head>
+    <body>
+        This is a test page. Really!!!!!!! 
+        <script src="test.js" type="text/javascript"></script>
+    </body>
+</html>
+"""
+
+css_content = """
+body{background-color: blue}
+/* This is just a content to blzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+ * zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+ * zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz */
+
+body{background-color: red}
+""" 
+
+js_content = """
+console.log("This is 1st");
+console.timeStamp("bla");
+/* This comment is here to take some space. GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGg
+ * sd50iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiifsdgsdgsdgfdfgdfg*/
+console.log("This is 2nd");
+console.timeStamp("bla");
+"""
+
+""" *********************************************************************** """
+class DelayRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """ Extensible proxy request handler. Extend by inheritance """
+
+    def __init__(self, *args, **kwargs):
+        BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+    def do_GET(self, post_data=None, is_head=False):
+        """Serve a GET request."""
+        css = "css" in self.path
+        js = "js" in self.path
+        delay = 0
+        if css:
+            content = css_content
+            delay = 5
+        elif js:
+            content = js_content
+            delay = 10
+        else:
+            content = html_content
+
+        if delay:
+            pre_content = content[:50]
+            post_content = content[50:]
+            self.send_response(200)
+            self.wfile.write(pre_content)
+            self.wfile.flush()
+            time.sleep(delay)
+            self.wfile.write(post_content)
+            self.wfile.flush()
+        else:
+            self.send_response(200)
+            self.wfile.write(content)
+            self.wfile.flush()
+
+
+class ThreadingServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    def __init__(self, address, handler):
+        BaseHTTPServer.HTTPServer.__init__(self, address, handler)
+
+def run(HandlerClass=DelayRequestHandler,
+             port=8002):
+    srv = ThreadingServer(("", port), HandlerClass)
+    print "serving requests on port", port
+    srv.serve_forever()
+
+if __name__ == '__main__':
+    run()
